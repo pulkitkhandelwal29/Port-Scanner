a936ef5b42f51f9f990fe331a76282198669b56a
Pulkit Khandelwal <67990422+pulkitkhandelwal29@users.noreply.github.com>
2021-10-19 08:12:55 +0530

    updated comments in script

--- portscanner.py
+++ portscanner.py
@@ -5,6 +5,7 @@ import termcolor
 
 
 def scan(target, ports):
+	#Mentioning scanning for which target
 	print('\n' + ' Starting Scan For ' + str(target))
 	for port in range(1,ports):
 		scan_port(target,port)
@@ -24,6 +25,7 @@ def scan_port(ipaddress, port):
 
 targets = input("[*] Enter Targets To Scan(split them by ,): ")
 ports = int(input("[*] Enter How Many Ports You Want To Scan: "))
+#if , in targets, then we will split
 if ',' in targets:
 	print(termcolor.colored(("[*] Scanning Multiple Targets"), 'green'))
 	for ip_addr in targets.split(','):
